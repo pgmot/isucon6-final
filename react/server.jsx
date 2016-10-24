4b241ad13bb3955720d5d7de9fa8b600cf0aaeec
Shusui MOYATANI <syusui.s@gmail.com>
2016-10-25 00:54:39 +0900

    nginxに対応させた

--- react/server.jsx
+++ react/server.jsx
@@ -1,5 +1,5 @@
 import express from 'express';
-import https from 'https';
+import http from 'http';
 import fs from 'fs';
 import path from 'path';
 import React from 'react';
@@ -20,17 +20,6 @@ const apiBaseUrl = process.env.API;
 if (!apiBaseUrl) {
   throw 'Please set environment variable API=http://...';
 }
-if (!process.env.SSL_KEY) {
-  throw 'Please set environment variable SSL_KEY=/path/to/server.key';
-}
-if (!process.env.SSL_CERT) {
-  throw 'Please set environment variable SSL_CERT=/path/to/server.crt';
-}
-
-const options = {
-  key: fs.readFileSync(process.env.SSL_KEY),
-  cert: fs.readFileSync(process.env.SSL_CERT),
-};
 
 const app = express();
 
@@ -104,8 +93,8 @@ app.get('*', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 443;
-https.createServer(options, app).listen(PORT);
+const PORT = process.env.PORT || 80;
+http.createServer(app).listen(PORT);
 
 function createHtml(appHtml, scriptTag, csrfToken) {
   return `<!DOCTYPE html>
